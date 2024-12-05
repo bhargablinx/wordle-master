d5331a8f3671e08bc3bdd212d5084de2f4eed9c1
bhargablinx <bhargabbhuyan001@gmail.com>
2024-12-05 16:53:12 +0530

    added prompt when trial is over

--- script.js
+++ script.js
@@ -27,17 +27,30 @@ async function validator(inputWord) {
     response = response.validWord;
     
     if (response) {
+        if (trialNumber == 0) return alert("Trial Over");;
         trialNumber--;
         console.log("Wail Validating...");
         const desiredWord = await wordOfTheDay();
-        if (desiredWord == inputWord)
+        if (desiredWord == inputWord){
+            alert("You Won!!!")
             return console.log("Win");
+        }
         else {
+            if (trialNumber > 0) getWordFromUser();
+            if (trialNumber == 0) return alert("Trial Over");
             console.log("Retry");
         }
     } else {
+        if (trialNumber == 0) return alert("Trial Over");;
+        if (trialNumber > 0) getWordFromUser();
         console.log("Invalid Word!");
     }
 }
 
-validator("glint");
+function getWordFromUser() {
+    console.log("Tral Remaining: " + trialNumber);
+    const gWord = prompt("Guess the word: ").toLocaleLowerCase();
+    validator(gWord);
+}
+
+getWordFromUser();
